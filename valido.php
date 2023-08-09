8e45f9ddd1f1e41274d445c937f79945bab75f82
José Junior Costa de Oliveira <4150@unieuro.int>
2023-08-09 16:28:19 -0300

    feat: SISTEMA WEB

---
+++ valido.php
@@ -0,0 +1,30 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Realizado Agendamento</title>
+    <style>
+        body
+        {
+            background-image: linear-gradient( to right,  rgb(45, 42, 26), rgb(29, 25, 93) ) ;
+            text-align: center;
+            font-family: Arial, Helvetica, sans-serif;
+            color: White;  
+        }
+
+        h1
+        {
+            position: absolute;
+            top: 25%;
+            left: 30%;
+        }
+
+
+    </style>
+</head>
+<body>
+    <h1> Horário marcado com sucesso!</h1><br><br>
+    <a href="agenda.php"> Voltar</a>
+    
+</body>
+</html>
